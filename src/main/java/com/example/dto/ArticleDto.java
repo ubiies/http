bc6bda2bb73aa11616eac00cf134b72355277f50
ubiies <alal55al9@naver.com>
2023-06-20 13:43:48 +0900

    refactor

--- src/main/java/com/example/dto/ArticleDto.java
+++ src/main/java/com/example/dto/ArticleDto.java
@@ -2,6 +2,8 @@ package com.example.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 // 블로그 게시글
 // 게시글 - 제목
 // 게시글 - 내용
@@ -12,8 +14,11 @@ import lombok.Data;
     "content": "content"
 }
 *    */
+
 @Data
 public class ArticleDto {
-    private String title;
-    private String content;
+    private String title; // 안녕하세요
+    private String content; /// 만나서 반갑습니다
+    private String writer;
+    private List<String> comments;
 }
